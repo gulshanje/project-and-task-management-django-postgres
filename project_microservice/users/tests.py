1c391f04d6ade538051f7e0655af9a9283ec622a
Gulshan <gulshan.kumar.dsa@gmail.com>
2025-03-04 03:06:47 +0200

    Added user test case

--- project_microservice/users/tests.py
+++ project_microservice/users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="testuser@example.com",
+            password="password123",
+            designation="QA Specialist"
+        )
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, "testuser")
+        self.assertEqual(self.user.email, "testuser@example.com")
+        self.assertTrue(self.user.check_password("password123"))
